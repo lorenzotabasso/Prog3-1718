10ae4921e9f3c457ef431eb3780e642e6b03e22f
Lorenzo Tabasso <tabasso.lorenzo@educ.di.unito.it>
2018-02-17 12:39:53 +0100

    added some MVC files

---
+++ src/Client.java
@@ -0,0 +1,31 @@
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author lorenzotabasso
+ */
+
+public class Client extends Application{
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    private void setView() {
+
+    }
+}
