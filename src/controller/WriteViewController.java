20e337d45269a4782fe629fda0a56cfee3d72ac0
Lorenzo Tabasso <tabasso.lorenzo@educ.di.unito.it>
2018-03-11 18:06:25 +0100

    experiments on WriteView

--- src/controller/WriteViewController.java
+++ src/controller/WriteViewController.java
@@ -1,7 +1,12 @@
 package controller;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ToolBar;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
 
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.net.URL;
@@ -9,7 +14,16 @@ import java.util.ResourceBundle;
 
 public class WriteViewController implements Initializable{
 
-    public WriteViewController() {}
+    public WriteViewController() {
+
+    }
+
+    @FXML
+    public VBox root;
+    public GridPane table;
+    public TextArea textContainer;
+    public ToolBar toolbar;
+
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
